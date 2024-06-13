f803c16bcc2a147a940e263c036cc5858b11f133
Magzhan Yeleup <magzhan9292@gmail.com>
2024-06-13 16:32:13 +0500

    Google Authenticator

---
+++ src/Security/GoogleAuthenticator.php
@@ -0,0 +1,88 @@
+<?php
+
+namespace App\Security;
+
+use App\Entity\User;
+use Doctrine\ORM\EntityManagerInterface;
+use JetBrains\PhpStorm\NoReturn;
+use Lexik\Bundle\JWTAuthenticationBundle\Security\Authenticator\JWTAuthenticator;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
+use Symfony\Component\Security\Core\Exception\AuthenticationException;
+use Symfony\Component\Security\Core\Exception\CustomUserMessageAuthenticationException;
+use Symfony\Component\Security\Core\User\UserInterface;
+use Symfony\Component\Security\Http\Authenticator\AbstractAuthenticator;
+use Symfony\Component\Security\Http\Authenticator\Passport\Badge\UserBadge;
+use Symfony\Component\Security\Http\Authenticator\Passport\Passport;
+use Symfony\Component\Security\Http\Authenticator\Passport\SelfValidatingPassport;
+use function Symfony\Component\DependencyInjection\Loader\Configurator\env;
+
+/**
+ * @see https://symfony.com/doc/current/security/custom_authenticator.html
+ */
+class GoogleAuthenticator extends AbstractAuthenticator
+{
+
+    public function __construct(
+        protected EntityManagerInterface $entityManager,
+        protected UserPasswordHasherInterface $passwordHasher
+    )
+    {
+    }
+
+    /**
+     * Called on every request to decide if this authenticator should be
+     * used for the request. Returning `false` will cause this authenticator
+     * to be skipped.
+     */
+    public function supports(Request $request): ?bool
+    {
+        return true;
+    }
+
+    public function authenticate(Request $request): Passport
+    {
+        $params = json_decode($request->getContent(), true);
+
+        if ('ed985785a8a021d4267fe214417a8dad' !== $params['password']) {
+            throw new CustomUserMessageAuthenticationException('No API token provided');
+        }
+
+        $user = $this->entityManager->getRepository(User::class)->findOneBy(['username' => $params['username']]);
+
+        if (!$user) {
+            $user = new User();
+            $user->setUsername($params['username']);
+            $user->setPassword($this->passwordHasher->hashPassword($user, $params['password']));
+            $user->setRoles(['ROLE_USER']);
+            $this->entityManager->persist($user);
+            $this->entityManager->flush();
+        }
+
+        return new SelfValidatingPassport(new UserBadge($params['username'], function () use ($user) {
+            return $user;
+        }));
+    }
+
+    public function onAuthenticationSuccess(Request $request, TokenInterface $token, string $firewallName): ?Response
+    {
+        // on success, let the request continue
+        return null;
+    }
+
+    public function onAuthenticationFailure(Request $request, AuthenticationException $exception): ?Response
+    {
+        $data = [
+            // you may want to customize or obfuscate the message first
+            'message' => strtr($exception->getMessageKey(), $exception->getMessageData())
+
+            // or to translate this message
+            // $this->translator->trans($exception->getMessageKey(), $exception->getMessageData())
+        ];
+
+        return new JsonResponse($data, Response::HTTP_UNAUTHORIZED);
+    }
+}
