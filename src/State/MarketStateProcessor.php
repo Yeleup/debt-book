3f4efa04f75117c0993a3cd81fbab3e5666bf1b0
Magzhan Yeleup <magzhan9292@gmail.com>
2024-06-19 11:38:49 +0500

    StateProcessor add employee to market

--- src/State/MarketStateProcessor.php
+++ src/State/MarketStateProcessor.php
@@ -4,8 +4,11 @@ namespace App\State;
 
 use ApiPlatform\Metadata\DeleteOperationInterface;
 use ApiPlatform\Metadata\Operation;
+use ApiPlatform\Metadata\Post;
 use ApiPlatform\State\ProcessorInterface;
+use App\Entity\Employee;
 use App\Entity\Market;
+use App\Repository\EmployeeRepository;
 use Symfony\Bundle\SecurityBundle\Security;
 
 class MarketStateProcessor implements ProcessorInterface
@@ -14,6 +17,7 @@ class MarketStateProcessor implements ProcessorInterface
         protected ProcessorInterface $persistProcessor,
         protected ProcessorInterface $removeProcessor,
         protected Security $security,
+        protected EmployeeRepository $employeeRepository,
     )
     {
     }
@@ -29,6 +33,14 @@ class MarketStateProcessor implements ProcessorInterface
     {
         if ($operation instanceof DeleteOperationInterface) {
             return $this->removeProcessor->process($data, $operation, $uriVariables, $context);
+        }
+
+        if ($operation instanceof Post) {
+            /** @var Employee $employee */
+            $employee = $this->employeeRepository->findOneBy(['user' => $this->security->getUser(), 'organization' => $data->getOrganization()]);
+            if ($employee) {
+                $data->addEmployee($employee);
+            }
         }
         return $this->persistProcessor->process($data, $operation, $uriVariables, $context);
     }
